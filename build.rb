06a929e6bf373a8487609c787c2910a88db79954
abhchand <me@abhchand.me>
2026-01-14 23:13:14 -0500

    build out basic calendar structure

--- build.rb
+++ build.rb
@@ -1,59 +1,56 @@
 #!/usr/bin/env ruby
 
-require 'bundler/inline'
+require 'active_support'
+require 'active_support/core_ext/numeric/time'
 require 'date'
-
-gemfile do
-  source 'https://rubygems.org'
-  gem 'activesupport', '~> 7.1'
-end
+require 'erb'
+require 'ostruct'
+require 'pry'
 
 week_starts_on = 'Wed'
 
 start_date = Date.new(Date.today.year, 1, 1)
 end_date = Date.new(Date.today.year, 12, 31)
 
-week_starts_on_idx = Date::ABBR_DAYNAMES.index('Wed')
+week_starts_on_idx = Date::ABBR_DAYNAMES.index(week_starts_on)
 start_date_idx = start_date.wday
+
+
+# Adjust start date so that the calendar begins on the desired "start of week"
+while(true) do
+  break if start_date.wday == week_starts_on_idx
+  start_date = start_date - 1
+end
 
 #
 # Build calendar structure
 #
 
-calendar = []
+calendar = 12.times.to_a.map { Array.new }
+
+# Initialize calendar with months
+
 (start_date..end_date).each do |date|
+  # We only care about days that are the start of the week
+  next unless date.wday == week_starts_on_idx
+
+  # If a week spans across a new month, we want to include this week in the
+  # NEW month, so we look at the last day
+  month_idx = (date + 6).month - 1
   week = { days: [], desc: [] }
 
-  if date.send("#{week_starts_on}?")
-    (date..(date + 6)).to_a.each do |day|
-      week[:days] << day
-    end
+  (date..(date + 6)).to_a.each do |day|
+    week[:days] << day
   end
-
-  calendar << week
+  calendar[month_idx] << week
 end
 
 #
-# Calculate Padded Days
+# Render Template
 #
 
-# Calculate the number of padded / fake days up front
-# e.g. If week starts on a tuesday, but year starts on a friday, we need 3 days
-# of padding
-num_padded_days =
-  case
-  when start_date_idx > week_starts_on_idx
-    start_date_idx - week_starts_on_idx
-  when start_date_idx < week_starts_on_idx
-    7 - week_starts_on_idx + start_date_idx
-  else
-    0
-  end
+template = File.read('index.html.erb')
+vars = { calendar: calendar }
 
-num_padded_days.times do |n|
-  calendar[0][:days].unshift(start_date - n.days)
-end
-
-
-require 'pry';
-binding.pry
+output = ERB.new(template).result(OpenStruct.new(vars).instance_eval { binding })
+File.open(__dir__ + '/doc/index.html', 'w') { |file| file.write(output) }
